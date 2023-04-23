04e7eb7d9cf3ad2d1053a5d1091d3c46f2297f05
douglasmatosdev <douglasmatosdev@gmail.com>
2023-04-23 19:05:19 -0300

    reset all

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
 
 import { data } from "@/utils";
 
 export default function Home() {
-  const [files, setFiles] = useState([]);
-
-  useEffect(() => {
-    const fetchFiles = async () => {
-      const accessToken = "AIzaSyAx-p3s64bZ4cj_UOtgZTx-Hs-bPwT4uI4";
-      const apiKey = "AIzaSyBvvroFtSdA9_jdbWK1f5b2qif3xkosWyE"
-      const folderId = "1Ea1Qr1_FHOvqL18GMTYQuEDRm5cQ1EDP";
-      const url = `https://www.googleapis.com/drive/v3/files?q='${folderId}'+in+parents&fields=*`;
-
-      const response = await fetch(url, {
-        headers: { Authorization: `Bearer ${accessToken}` },
-      }).then(r => {
-
-        console.log(r)
-        return r
-      })
-      const data = await response.json();
-      setFiles(data.files);
-    };
-
-    fetchFiles();
-  }, []);
-
   return (
     <main className="flex flex-col border border-red-100">
       <section className="flex justify-between">
-        {/* {data.map((book, index) => {
+        {data.map((book, index) => {
           return (
             <div
               key={`${book.title}_${index}`}
@@ -43,15 +19,7 @@ export default function Home() {
               <span>{book.publishing_company}</span>
             </div>
           );
-        })} */}
-        <div>
-          <h1>My Google Drive Files</h1>
-          <ul>
-            {files?.map((file) => (
-              <li key={file.id}>{file.name}</li>
-            ))}
-          </ul>
-        </div>
+        })}
       </section>
       <footer className="flex justify-center fixed bottom-0 items-center w-[100%] pb-4">
         <div className="flex justify-between items-center w-80">
